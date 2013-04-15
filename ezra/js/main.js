bd9cdfc4533c264c6c478deeedce7c30e2ba0d6c
Jason Levinsohn <jason.levinsohn@gmail.com>
2013-04-15 15:20:48 -0400

    started the javascript for the image templates

--- ezra/js/main.js
+++ ezra/js/main.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
 
 
 });
+
+$(function() {
+    var $ibWrapper = $('#ib-main-wrapper'),
+        
+        Template = (function() {
+            var kinectic_moving = false,
+                current         = -1,
+                isAnimating     = false,
+                $ibItems        = $ibWrapper.find('div.ib-main > a'),
+                $ibImgItems     = $ibItems.not('.ib-content'),
+                imgItemsCount   = $ibImgItems.length,
+                init            = function() {
+                    console.log("sup");
+                };
+            
+            return { init : init };
+        })();
+
+        Template.init();
+});
